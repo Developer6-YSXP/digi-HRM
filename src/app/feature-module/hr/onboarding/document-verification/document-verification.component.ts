d25aa64b9d23f9855b728793b27e5b5a1bce80c9
Bhupender Singh <developer6.india@yellowstonexps.com>
2024-10-01 10:15:50 +0530

    create onboarding component

---
+++ src/app/feature-module/hr/onboarding/document-verification/document-verification.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-document-verification',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './document-verification.component.html',
+  styleUrl: './document-verification.component.scss'
+})
+export class DocumentVerificationComponent {
+
+}
