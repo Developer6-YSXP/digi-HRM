d08250692cd34736a9173a0f6b67f171d305ad32
Anuj Pal <developer5.india@yellowstonexps.com>
2024-10-04 15:36:48 +0530

    Implemented Submenu

--- src/app/feature-module/hr/onboarding/appointment-schedule/appointment-schedule.component.ts
+++ src/app/feature-module/hr/onboarding/appointment-schedule/appointment-schedule.component.ts
@@ -1,9 +1,25 @@
-import { Component, inject, OnInit,  ViewChild, ElementRef  } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  Component,
+  inject,
+  OnInit,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
 import { LocalService } from 'src/app/core/services/local/local.service';
 import { Sort } from '@angular/material/sort';
-import { DataService, apiResultFormat, getPolicies, routes } from 'src/app/core/core.index';
+import {
+  DataService,
+  apiResultFormat,
+  getPolicies,
+  routes,
+} from 'src/app/core/core.index';
 import { OnboardingService } from '../onboarding.service';
 declare var bootstrap: any;
 interface Appointment {
@@ -25,53 +41,50 @@ interface Reminder {
   styleUrl: './appointment-schedule.component.scss',
 })
 export class AppointmentScheduleComponent implements OnInit {
-  @ViewChild('deleteModal') deleteModal!: ElementRef; 
+  @ViewChild('deleteModal') deleteModal!: ElementRef;
   onboardingForm!: FormGroup;
   editForm!: FormGroup;
   selectedAppointmentIndex: number | null = null;
-  appointmentToDelete: Appointment | null = null; 
+  appointmentToDelete: Appointment | null = null;
   dataSource!: MatTableDataSource<getPolicies>;
-  appointments: Appointment[] = []; 
-    // pagination variables
-    public lastIndex = 0;
-    public pageSize = 10;
-    public totalData = 0;
-    public skip = 0;
-    public limit: number = this.pageSize;
-    public pageIndex = 0;
-    public serialNumberArray: Array<number> = [];
-    public currentPage = 1;
-    public pageNumberArray: Array<number> = [];
-    public pageSelection: Array<pageSelection> = [];
-    public totalPages = 0;
-    role! : number;
-    //** / pagination variables
-    public allAppointment: Array<getPolicies> = [];
+  appointments: Appointment[] = [];
+  // pagination variables
+  public lastIndex = 0;
+  public pageSize = 10;
+  public totalData = 0;
+  public skip = 0;
+  public limit: number = this.pageSize;
+  public pageIndex = 0;
+  public serialNumberArray: Array<number> = [];
+  public currentPage = 1;
+  public pageNumberArray: Array<number> = [];
+  public pageSelection: Array<pageSelection> = [];
+  public totalPages = 0;
+  role!: number;
+  //** / pagination variables
+  public allAppointment: Array<getPolicies> = [];
   public routes = routes;
   private onboardingService = inject(OnboardingService);
- 
-  constructor(private fb: FormBuilder,
-    private data: DataService,
-  ) {}
+
+  constructor(private fb: FormBuilder, private data: DataService) {}
 
   ngOnInit(): void {
-     
     this.onboardingForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       appointmentDate: ['', Validators.required],
       appointmentTime: ['', Validators.required],
       method: ['', Validators.required],
-      department: ['', Validators.required]
+      department: ['', Validators.required],
     });
 
-     this.editForm = this.fb.group({
+    this.editForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       appointmentDate: ['', Validators.required],
       appointmentTime: ['', Validators.required],
       method: ['', Validators.required],
-      department: ['', Validators.required]
+      department: ['', Validators.required],
     });
     this.reminderForm = this.fb.group({
       name: ['', Validators.required],
@@ -100,12 +113,13 @@ export class AppointmentScheduleComponent implements OnInit {
     if (this.editForm.valid && this.selectedAppointmentIndex !== null) {
       this.appointments[this.selectedAppointmentIndex] = this.editForm.value;
       this.editForm.reset();
-      this.selectedAppointmentIndex = null;  
+      this.selectedAppointmentIndex = null;
     }
   }
-
   
- 
+  
+  
+  
   onSubmit(): void {
     if (this.onboardingForm.valid) {
       const appointmentData = this.onboardingForm.value;
@@ -120,16 +134,16 @@ export class AppointmentScheduleComponent implements OnInit {
       );
     }
   }
-  
-// Method to open edit modal and populate the form
-// public populateEditForm(document: any): void {
-//   this.selectedDocument = document; 
-//   this.editEmployeeForm.patchValue({
-//     client: document.type,
-//     status: document.status,
-//     file: document.file
-//   });
-// }
+
+  // Method to open edit modal and populate the form
+  // public populateEditForm(document: any): void {
+  //   this.selectedDocument = document;
+  //   this.editEmployeeForm.patchValue({
+  //     client: document.type,
+  //     status: document.status,
+  //     file: document.file
+  //   });
+  // }
 
   public changePageSize(): void {
     this.pageSelection = [];
@@ -152,95 +166,92 @@ export class AppointmentScheduleComponent implements OnInit {
           this.serialNumberArray.push(serialNumber);
         }
       });
-      this.dataSource = new MatTableDataSource<getPolicies>(this.allAppointment);
+      this.dataSource = new MatTableDataSource<getPolicies>(
+        this.allAppointment
+      );
       this.calculateTotalPages(this.totalData, this.pageSize);
     });
-}
-private calculateTotalPages(totalData: number, pageSize: number): void {
-  this.pageNumberArray = [];
-  this.totalPages = totalData / pageSize;
-  if (this.totalPages % 1 !== 0) {
-    this.totalPages = Math.trunc(this.totalPages + 1);
   }
-  for (let i = 1; i <= this.totalPages; i++) {
-    const limit = pageSize * i;
-    const skip = limit - pageSize;
-    this.pageNumberArray.push(i);
-    this.pageSelection.push({ skip: skip, limit: limit });
+  private calculateTotalPages(totalData: number, pageSize: number): void {
+    this.pageNumberArray = [];
+    this.totalPages = totalData / pageSize;
+    if (this.totalPages % 1 !== 0) {
+      this.totalPages = Math.trunc(this.totalPages + 1);
+    }
+    for (let i = 1; i <= this.totalPages; i++) {
+      const limit = pageSize * i;
+      const skip = limit - pageSize;
+      this.pageNumberArray.push(i);
+      this.pageSelection.push({ skip: skip, limit: limit });
+    }
   }
-}
-public moveToPage(pageNumber: number): void {
-  this.currentPage = pageNumber;
-  this.skip = this.pageSelection[pageNumber - 1].skip;
-  this.limit = this.pageSelection[pageNumber - 1].limit;
-  if (pageNumber > this.currentPage) {
-    this.pageIndex = pageNumber - 1;
-  } else if (pageNumber < this.currentPage) {
-    this.pageIndex = pageNumber + 1;
+  public moveToPage(pageNumber: number): void {
+    this.currentPage = pageNumber;
+    this.skip = this.pageSelection[pageNumber - 1].skip;
+    this.limit = this.pageSelection[pageNumber - 1].limit;
+    if (pageNumber > this.currentPage) {
+      this.pageIndex = pageNumber - 1;
+    } else if (pageNumber < this.currentPage) {
+      this.pageIndex = pageNumber + 1;
+    }
+    this.getTableData();
   }
-  this.getTableData();
-}
 
   public sortData(sort: Sort) {
-  const data = this.allAppointment.slice();
+    const data = this.allAppointment.slice();
 
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  if (!sort.active || sort.direction === '') {
-    this.allAppointment = data;
-  } else {
-    this.allAppointment = data.sort((a: any, b: any) => {
-      const aValue = (a as any)[sort.active];
-      const bValue = (b as any)[sort.active];
-      return (aValue < bValue ? -1 : 1) * (sort.direction === 'asc' ? 1 : -1);
-    });
-  }
-}
-public searchData(value: string): void {
-  this.dataSource.filter = value.trim().toLowerCase();
-  this.allAppointment = this.dataSource.filteredData;
-}
-public getMoreData(event: string): void {
-  if (event === 'next') {
-    this.currentPage++;
-    this.pageIndex = this.currentPage - 1;
-    this.limit += this.pageSize;
-    this.skip = this.pageSize * this.pageIndex;
-    this.getTableData();
-  } else if (event === 'previous') {
-    this.currentPage--;
-    this.pageIndex = this.currentPage - 1;
-    this.limit -= this.pageSize;
-    this.skip = this.pageSize * this.pageIndex;
-    this.getTableData();
-  }
-}
-// Function to delete an appointment
-deleteAppointment(appointment: Appointment) {
-  this.appointmentToDelete = appointment;  
-}
-confirmDelete() {
-  if (this.appointmentToDelete) {
-    const index = this.appointments.indexOf(this.appointmentToDelete);
-    if (index > -1) {
-      this.appointments.splice(index, 1);
-      this.appointmentToDelete = null;  
-    }
-    else{
-      const modelElement = this.deleteModal.nativeElement;
-      const modal = bootstrap.Modal.getInstance(modelElement);
-  if (modal) {
-    modal.hide();  
-  }
+     
+    if (!sort.active || sort.direction === '') {
+      this.allAppointment = data;
+    } else {
+      this.allAppointment = data.sort((a: any, b: any) => {
+        const aValue = (a as any)[sort.active];
+        const bValue = (b as any)[sort.active];
+        return (aValue < bValue ? -1 : 1) * (sort.direction === 'asc' ? 1 : -1);
+      });
     }
   }
-}
-reminderForm!: FormGroup;
+  public searchData(value: string): void {
+    this.dataSource.filter = value.trim().toLowerCase();
+    this.allAppointment = this.dataSource.filteredData;
+  }
+  public getMoreData(event: string): void {
+    if (event === 'next') {
+      this.currentPage++;
+      this.pageIndex = this.currentPage - 1;
+      this.limit += this.pageSize;
+      this.skip = this.pageSize * this.pageIndex;
+      this.getTableData();
+    } else if (event === 'previous') {
+      this.currentPage--;
+      this.pageIndex = this.currentPage - 1;
+      this.limit -= this.pageSize;
+      this.skip = this.pageSize * this.pageIndex;
+      this.getTableData();
+    }
+  }
+  // Function to delete an appointment
+  deleteAppointment(appointment: Appointment) {
+    this.appointmentToDelete = appointment;
+  }
+  confirmDelete() {
+    if (this.appointmentToDelete) {
+      const index = this.appointments.indexOf(this.appointmentToDelete);
+      if (index > -1) {
+        this.appointments.splice(index, 1);
+        this.appointmentToDelete = null;
+      } else {
+        const modelElement = this.deleteModal.nativeElement;
+        const modal = bootstrap.Modal.getInstance(modelElement);
+        if (modal) {
+          modal.hide();
+        }
+      }
+    }
+  }
+  reminderForm!: FormGroup;
   reminders: Reminder[] = [];
   appointmentToEdit: Reminder | null = null;
-
-   
-
-  
 
   addReminder(): void {
     if (this.reminderForm.valid) {
@@ -256,7 +267,7 @@ reminderForm!: FormGroup;
   }
 
   deleteReminder(reminder: Reminder): void {
-    this.reminders = this.reminders.filter(r => r !== reminder);
+    this.reminders = this.reminders.filter((r) => r !== reminder);
   }
 }
 export interface pageSelection {
--- src/app/feature-module/hr/onboarding/onboarding-routing.module.ts
+++ src/app/feature-module/hr/onboarding/onboarding-routing.module.ts
@@ -4,6 +4,14 @@ import { DocumentVerificationComponent } from './document-verification/document-
 import { OnboardingComponent } from './onboarding.component';
 import { AppointmentScheduleComponent } from './appointment-schedule/appointment-schedule.component';
 import { OfferLetterComponent } from './offer-letter/offer-letter.component';
+import { ConfirmationProcessComponent } from './confirmation-process/confirmation-process.component';
+import { HirePortalComponent } from './hire-portal/hire-portal.component';
+import { InterviewFeedbackComponent } from './interview-feedback/interview-feedback.component';
+import { MultiPayrollComplianceComponent } from './multi-payroll-compliance/multi-payroll-compliance.component';
+import { OnboardingChecklistManagementComponent } from './onboarding-checklist-management/onboarding-checklist-management.component';
+import { PayrollConfigurationInterfaceComponent } from './payroll-configuration-interface/payroll-configuration-interface.component';
+import { ReportingAnalyticsComponent } from './reporting-analytics/reporting-analytics.component';
+import { TaskAutomationComponent } from './task-automation/task-automation.component';
 
 const routes: Routes = [
   {
@@ -12,7 +20,15 @@ const routes: Routes = [
     children:[
        { path:'document-verify',  component: DocumentVerificationComponent },
        { path: 'appointment', component:AppointmentScheduleComponent },
-       { path: 'offer-letter', component: OfferLetterComponent }
+       { path: 'offer-letter', component: OfferLetterComponent },
+       { path: 'confirmation', component: ConfirmationProcessComponent },
+       { path: 'interview-feedback', component: InterviewFeedbackComponent },
+       { path: 'multi-payroll', component: MultiPayrollComplianceComponent },
+       { path: 'payroll-confirguration', component: PayrollConfigurationInterfaceComponent },
+       { path: 'onboarding-checklist', component: OnboardingChecklistManagementComponent },
+       { path: 'hire-portal', component: HirePortalComponent },
+       { path: 'reporting-analytics', component: ReportingAnalyticsComponent },
+       { path: 'task-automation', component: TaskAutomationComponent },
     ]
   }
 ];
